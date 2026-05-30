5aba2d43c60efc165a1b82874e6eb051ce63c31b
Avesh Maurya <aveshmaurya9453@gmail.com>
2026-05-30 08:15:09 +0530

    Connect product routes to express app

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ import "dotenv/config";
 // import dotenv from "dotenv";
 // dotenv.config();
 import express from "express";
+import productRoutes from "./routes/product.routes.js";
 
 const app = express();
 
 // middlewares
 app.use(express.json());
+
+app.use("/api/products", productRoutes);
 
 
 export default app;
