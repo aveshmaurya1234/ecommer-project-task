5619b890a8379f5abc54f8f0bbe6a073f5cb2b11
Avesh Maurya <aveshmaurya9453@gmail.com>
2026-05-30 14:17:15 +0530

    implement add pre-save password hashing and comparePassword method in model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,27 +1,46 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
+    {
+        name: {
+            type: String,
+            required: true,
+        },
 
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
 
-    password: {
-      type: String,
-      required: true,
+        password: {
+            type: String,
+            required: true,
+        },
     },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
+
+// Hash password before saving in database
+
+userSchema.pre("save", async function () {
+  // prevent re-hashing
+  if (!this.isModified("password")) {
+    return;
+  };
+
+  this.password = await bcrypt.hash(this.password, 10);
+}); 
+
+
+// Compare password method
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
 
 const User = mongoose.model("User", userSchema);
 
